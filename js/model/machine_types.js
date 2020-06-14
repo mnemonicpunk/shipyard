b384ddcc42704f0a7ae1279168b82a35e70d2637
Joshua Bendig <cyberpunk@knuddels.de>
2020-06-14 22:47:36 +0200

    added graohics for several placeholders

--- js/model/machine_types.js
+++ js/model/machine_types.js
@@ -410,8 +410,28 @@ const MACHINE_TYPES = [
         height: 1,
         has_direction_arrows: true,
         sprite: function(node, direction) {
-            let p = node.addChild(new SpritePlaceholder());
-            p.label = "Smart Grabber";
+            node.addPart("gameatlas4.png", "device-grabber-smart-base", -36, -36);
+            a = node.addPart("gameatlas4.png", "device-grabber-smart-arm-part-1", 0, -22);
+            a.image_data.x_off = -6;
+            a.image_data.y_off = -20;
+            a.rotation = direction;
+
+            a = a.addPart("gameatlas5.png", "device-grabber-smart-arm-part-2", 55, -20);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+
+            let arm = a;
+
+            a = arm.addPart("gameatlas5.png", "device-grabber-finger", 85, 35);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+            a.rotation = 180;
+
+            a = arm.addPart("gameatlas5.png", "device-grabber-finger", 85, -15);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+            a.scale_y = -1;
+            a.rotation = 180;
         }                     
     },      
     {
@@ -419,11 +439,25 @@ const MACHINE_TYPES = [
         type: "smart_splitter",
         width: 1,
         height: 1,
-        has_direction_arrows: true,
+        has_direction_arrows: false,
         sprite: function(node, direction) {
-            let p = node.addChild(new SpritePlaceholder());
-            p.label = "Smart Splitter";
-        }                     
+            //node.addPart("gameatlas3.png", "device-splitter-body-horizontal-down", -54, -82);
+            switch (direction) {
+                case (DIRECTIONS.up):
+                    part = node.addPart("gameatlas3.png", "device-splitter-smart-body-horizontal-down", -54, -82);
+                    break;
+                case (DIRECTIONS.down):                
+                    part = node.addPart("gameatlas3.png", "device-splitter-smart-body-horizontal-up", -54, -82);
+                    break;
+                case (DIRECTIONS.left):
+                    part = node.addPart("gameatlas3.png", "device-splitter-smart-body-horizontal-vertical", 54, -82);
+                    part.scale_x = -1;
+                    break;
+                case (DIRECTIONS.right):
+                    part = node.addPart("gameatlas3.png", "device-splitter-smart-body-horizontal-vertical", -54, -82);
+                    break;                    
+            }
+        },                   
     },  
     {
         name: "Crusher",
@@ -454,8 +488,28 @@ const MACHINE_TYPES = [
         height: 1,
         has_direction_arrows: true,
         sprite: function(node, direction) {
-            let p = node.addChild(new SpritePlaceholder());
-            p.label = "Fast Grabber";
+            node.addPart("gameatlas4.png", "device-grabber-fast-base", -36, -36);
+            a = node.addPart("gameatlas4.png", "device-grabber-fast-arm-part-1", 0, -22);
+            a.image_data.x_off = -6;
+            a.image_data.y_off = -20;
+            a.rotation = direction;
+
+            a = a.addPart("gameatlas5.png", "device-grabber-fast-arm-part-2", 55, -20);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+
+            let arm = a;
+
+            a = arm.addPart("gameatlas5.png", "device-grabber-finger", 85, 35);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+            a.rotation = 180;
+
+            a = arm.addPart("gameatlas5.png", "device-grabber-finger", 85, -15);
+            a.image_data.x_off = 0;
+            a.image_data.y_off = 0;
+            a.scale_y = -1;
+            a.rotation = 180;
         }                     
     },    
     {
@@ -470,26 +524,111 @@ const MACHINE_TYPES = [
         }                     
     },    
     {
-        name: "Fast Belt",
-        type: "fast_belt",
+        name: "Fast Belt (straight)",
+        type: "fast_belt_straight",
+        width: 1,
+        height: 1,
+        has_direction_arrows: false,
+        sprite: function(node, direction) {
+            let part = null;
+            let part_arrow = null;
+    
+            switch (direction) {
+                case (DIRECTIONS.up):
+                    part = node.addPart("gameatlas4.png", "south-straight", -75, -75);
+                    part_arrow = node.addPart("gameatlas4.png", "south-straight-arrow-fast", -75, -85);
+                    break;
+                case (DIRECTIONS.down):                
+                    part = node.addPart("gameatlas3.png", "north-straight", -65, -75);
+                    part_arrow = node.addPart("gameatlas4.png", "north-straight-arrow-fast", -65, -65);
+                    break;
+                case (DIRECTIONS.left):
+                    part = node.addPart("gameatlas4.png", "west-straight", -75, -48);
+                    part_arrow = node.addPart("gameatlas4.png", "west-straight-arrow-fast", -75, -75);
+                    break;
+                case (DIRECTIONS.right):
+                    part = node.addPart("gameatlas3.png", "east-straight", -75, -43);
+                    part_arrow = node.addPart("gameatlas3.png", "east-straight-arrow-fast", -75, -70);
+                    break;                    
+            }
+    
+            part.scale_x = 1.5;
+            part.scale_y = 1.5;
+            part_arrow.scale_x = 1.5;
+            part_arrow.scale_y = 1.5;            
+        }
+    },
+    {
+        name: "Fast Belt (curved)",
+        type: "fast_belt_curved",
+        width: 1,
+        height: 1,
+        has_direction_arrows: false,
+        sprite: function(node, direction) {
+            let part = null;
+            let part_arrow = null;
+    
+            switch (direction) {
+                case (DIRECTIONS.up):
+                    part = node.addPart("gameatlas4.png", "south-CCW", -75, -35);
+                    part_arrow = node.addPart("gameatlas4.png", "south-CCW-arrow-fast", -75, -75);
+                    break;
+                case (DIRECTIONS.down):                
+                    part = node.addPart("gameatlas3.png", "north-CCW", -65, -90);
+                    part_arrow = node.addPart("gameatlas4.png", "north-CCW-arrow-fast", -65, -70);
+                    break;
+                case (DIRECTIONS.left):
+                    part = node.addPart("gameatlas5.png", "west-CCW", -65, -38);
+                    part_arrow = node.addPart("gameatlas4.png", "west-CCW-arrow-fast", -65, -75);
+                    break;                    
+                case (DIRECTIONS.right):
+                    part = node.addPart("gameatlas4.png", "east-CCW", -75, -88);
+                    part_arrow = node.addPart("gameatlas3.png", "east-CCW-arrow-fast", -75, -68);
+                    break;                                        
+
+            }
+    
+            part.scale_x = 1.5;
+            part.scale_y = 1.5;
+            part_arrow.scale_x = 1.5;
+            part_arrow.scale_y = 1.5;            
+        }
+    },
+    {
+        name: "Fast Belt (t-shape)",
+        type: "fast_belt_t",
         width: 1,
         height: 1,
         has_direction_arrows: true,
         sprite: function(node, direction) {
             let p = node.addChild(new SpritePlaceholder());
-            p.label = "Fast Belt";
-        }                     
+            p.label = "Fast Belt (T-Shape)";
+        }
     },    
     {
         name: "Fast Splitter",
         type: "fast_splitter",
         width: 1,
         height: 1,
-        has_direction_arrows: true,
+        has_direction_arrows: false,
         sprite: function(node, direction) {
-            let p = node.addChild(new SpritePlaceholder());
-            p.label = "Fast Splitter";
-        }                     
+            //node.addPart("gameatlas3.png", "device-splitter-body-horizontal-down", -54, -82);
+            switch (direction) {
+                case (DIRECTIONS.up):
+                    part = node.addPart("gameatlas3.png", "device-splitter-fast-body-horizontal-down", -54, -82);
+                    break;
+                case (DIRECTIONS.down):                
+                    part = node.addPart("gameatlas3.png", "device-splitter-fast-body-horizontal-up", -54, -82);
+                    break;
+                case (DIRECTIONS.left):
+                    part = node.addPart("gameatlas3.png", "device-splitter-fast-body-horizontal-vertical", 54, -82);
+                    part.scale_x = -1;
+                    break;
+                case (DIRECTIONS.right):
+                    part = node.addPart("gameatlas3.png", "device-splitter-fast-body-horizontal-vertical", -54, -82);
+                    break;                    
+            }
+        },                     
     },    
     {
         name: "Heat Gun",
